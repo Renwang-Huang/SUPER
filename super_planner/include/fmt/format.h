c2a772e893e7bebc6c9cfbe98fd0768647fa7f4b
Renwang Huang <RenwangHuangX@gmail.com>
2026-04-13 16:59:45 +0800

    修复isnan造成的编译问题

--- super_planner/include/fmt/format.h
+++ super_planner/include/fmt/format.h
@@ -2491,7 +2491,7 @@ FMT_CONSTEXPR20 auto write_float(OutputIt out, const DecimalFP& f,
 }
 
 template <typename T> constexpr bool isnan(T value) {
-  return !(value >= value);  // std::isnan doesn't support __float128.
+  return !(value >= value);  // isnan doesn't support __float128.
 }
 
 template <typename T, typename Enable = void>
@@ -3251,7 +3251,7 @@ FMT_CONSTEXPR20 auto write(OutputIt out, T value) -> OutputIt {
   using uint = typename dragonbox::float_info<floaty>::carrier_uint;
   uint mask = exponent_mask<floaty>();
   if ((bit_cast<uint>(value) & mask) == mask)
-    return write_nonfinite(out, std::isnan(value), specs, fspecs);
+    return write_nonfinite(out, isnan(value), specs, fspecs);
 
   auto dec = dragonbox::to_decimal(static_cast<floaty>(value));
   return write_float(out, dec, specs, fspecs, {});
